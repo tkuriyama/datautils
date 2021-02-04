652072826b0f7753cd88daa77a1337d572369b66
tkuriyama <taro.kuriyama@gmail.com>
2021-02-03 20:52:22 -0500

    Adding logger calls to email_lib

--- datautils/core/email_lib.py
+++ datautils/core/email_lib.py
@@ -2,9 +2,17 @@
 """
 
 from email.mime.text import MIMEText # type: ignore
+import logging # type: ignore
 from smtplib import SMTP_SSL as SMTP # type: ignore
 import sys # type: ignore
 from typing import (List, TypedDict) # type: ignore
+
+from datautils.core import log_setup # type: ignore
+
+################################################################################
+# Initialize Logging -- set logging level to > 50 to suppress all output
+
+logger = log_setup.init_file_log(__name__, logging.INFO)
 
 ################################################################################
 
@@ -42,7 +50,10 @@ def send(subject: str,
 
     try:
         conn.sendmail(fromAddr, ','.join(toAddrs), msg.as_string())
+        logger.info('Email sent')
     except Exception as e:
+        logger.error('Email send failed: {}'.format(e))
         sys.exit('Send failed: {}'.format(e))
     finally:
         conn.quit()
+        logger.debug('SMTP connection closed')
