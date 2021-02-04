399c67cf44f1ad69f3188e66540ee3fc886f36a4
tkuriyama <taro.kuriyama@gmail.com>
2021-02-04 17:34:47 -0500

    Version 0.2.0 incorporating revised jbro

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 setup(
     name="datautils",
-    version="0.1.0",
+    version="0.2.0",
     packages=['datautils', 'datautils.core', 'datautils.ingest',
               'datautils.json'],
     scripts=['datautils/bin/jbro'],
