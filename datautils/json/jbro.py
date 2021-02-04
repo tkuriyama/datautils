de354d967b54fb8c1b1dabf7dfb916804886e3ae
tkuriyama <taro.kuriyama@gmail.com>
2021-02-04 16:18:35 -0500

    Rewrites to jbro to support list of dicts

--- datautils/json/jbro.py
+++ datautils/json/jbro.py
@@ -1,32 +1,201 @@
-# JSON Browser
-# Simple command-line utility for inspecting contents of JSON files
+""" JSON Browser
+Simple command-line utility for inspecting contents of JSON files
+"""
 
-from subprocess import Popen, PIPE
-import json
+import json # type: ignore
+import logging # type: ignore
+from subprocess import Popen, PIPE # type: ignore
+from typing import List, Optional # type: ignore
 
+from datautils.core import log_setup # type: ignore
+from datautils.core import utils # type: ignore
+
+################################################################################
+# Initialize Logging -- set logging level to > 50 to suppress all output
+
+logger = log_setup.init_file_log(__name__, logging.WARNING)
+
+################################################################################
+
+def main(args):
+    """Process args from argparse."""
+    data = test_json(args.filename)
+    if not data: return
+
+    if args.describe:
+        describe(data, args.quiet)
+    if args.sample:
+        sample(data, args.sample, args.quiet, args.truncate)
+    if args.chars:
+        get_chars(data, args.chars, args.quiet)
+    if args.find:
+        find(data, args.find, args.quiet, args.truncate)
+    if args.find_recursive:
+        find_rec(data, args.find_recursive, args.quiet, args.truncate)
+    if args.keys:
+        get_keys(data, args.quiet, args.truncate)
+    if args.keys_recursive:
+        get_all_keys(data, args.quiet, args.truncate)
+
+    print('\n')
+
+    other_args = [args.describe, args.sample, args.chars, args.find,
+                  args.find_recursive, args.keys, args.keys_recursive]
+    if args.less or not any(other_args):
+        data_str = json.dumps(data, indent=2, sort_keys=True)
+        print(data_str)
+        less(data_str)
+
+################################################################################
+# Inspection Functions
+
+def describe(data: dict, quiet: bool) -> None:
+    """Describe key attributes of data."""
+    print('\n> Describe structure of file' if not quiet else '\n')
+
+    print('Top-level keys {:,d}'.format(len(data)))
+    print('Total keys     {:,d}'.format(count_keys(data)))
+    print('Max depth      {:,d}'.format(max_depth(data)))
+    print('Total chars    {:,d}'.format(len(json.dumps(data))))
+
+def sample(data: dict, n: int, quiet: bool, truncate: bool) -> None:
+    """Sample first n (key, value) pairs."""
+    msg = '\n> Sample first {:,d} (key, value) pairs'.format(n)
+
+    print(msg if not quiet else '\n')
+    keys = list(data)[:n]
+    pairs = [join_pair(key, data[key], truncate) for key in keys]
+    print('\n'.join(pairs))
+
+def get_chars(data: dict, n: int, quiet: bool) -> None:
+    """Print first n chars."""
+    print('\n> Show first {:,d} chars'.format(n) if not quiet else '\n')
+    data_str = json.dumps(data, indent=2, sort_keys=True)
+    print(data_str[:n])
+
+def get_keys(data: dict, quiet: bool, truncate: bool) -> None:
+    """List all top-level keys in data."""
+    print('\n> List all top-level keys' if not quiet else '\n')
+
+    print_freq_keys(list(data), truncate)
+
+def get_all_keys(search_d: dict, quiet: bool, truncate: bool) -> None:
+    """Retrieve all keys in dict (BFS) in format key1.key2..."""
+    print('\n> List all keys' if not quiet else '\n')
+
+    all_keys = []
+    dicts = [('', search_d)]
+
+    while dicts:
+        parent, d = dicts.pop()
+        for key in d.keys():
+            full_key = key if parent == '' else '.'.join([parent, key])
+            all_keys.append(full_key)
+            if isinstance(d[key], dict):
+                dicts.append((full_key, d[key]))
+            elif isinstance(d[key], list):
+                dicts.extend((full_key, d_) for d_ in d[key]
+                             if isinstance(d_, dict))
+
+    print_freq_keys(all_keys, truncate)
+
+################################################################################
+# Search Functions
+
+def find(data: dict, key: str, quiet: bool, truncate: bool) -> None:
+    """Attempt to find key in dict, where nested key in form key1.key2..."""
+    print('\n> Find key {}'.format(key) if not quiet else '\n')
+
+    val = _find_key(data, key.split('.'))
+    if val is not None:
+        if truncate:
+            print(trim(val, 80))
+        elif isinstance(val, dict) or isinstance(val, list):
+            print(json.dumps(val, indent=2, sort_keys=True))
+        else:
+            print(val)
+    else:
+        print('Key not found.')
+
+def _find_key(d: dict, keys: List[str]):
+    """Implementation of find().
+    Attempt to find the nested key key1.key2... and return its value.
+    May return multiple values if the leaf key occurs in a list.
+    """
+    key = keys[0]
+
+    return (d[key] if len(keys) == 1 and key in d else
+            None if len(keys) == 1 and key not in d else
+            _find_key_list(d[key], keys[1:]) if isinstance(d[key], list) else
+            None if not isinstance(d[key], dict) else
+            _find_key(d[key], keys[1:]) if key in d else
+            None)
+
+def _find_key_list(lst: list, keys: List[str]) -> list:
+    """Map _find_key over list elems that are dicts."""
+    return [_find_key(elem, keys) for elem in lst if isinstance(elem, dict)]
+
+def find_rec(data: dict, key: str, quiet: bool, truncate: bool) -> None:
+    """Find (unnested ) key recursively in data, return all occurences."""
+    print('\n> Find key {} recursively'.format(key) if not quiet else '\n')
+
+    vals = _find_key_rec(data, key)
+    if vals:
+        found = [join_pair('Level {:,d}'.format(lvl), val, truncate)
+                 for lvl, val in vals]
+        print('\n'.join(found))
+    else:
+        print('Key not found.')
+
+def _find_key_rec(search_d: dict, search_key: str):
+    """Implementation of find_rec().
+    Attempt to find all instnaces of the unnested search_key (BFS) in dict.
+    Return all found values and corresponding levels of nesting.
+    """
+    hits = []
+    dicts = [(0, search_d)]
+
+    while dicts:
+        level, d = dicts.pop()
+        for key in d:
+            if key == search_key:
+                hits.append((level, d[key]))
+            else:
+                if isinstance(d[key], dict):
+                    dicts.append((level + 1, d[key]))
+                elif isinstance(d[key], list):
+                    dicts.extend((level + 1, d_) for d_ in d[key]
+                                 if isinstance(d_, dict))
+
+    return hits
+
+################################################################################
 # Helpers
 
-def test_json(filename):
-    """Verify that given filename is valid JSON; if not, return None."""
+def test_json(fname: str) -> Optional[dict]:
+    """Return either JSON or None (if JSON parse fails)."""
+    data: Optional[dict]
     try:
-        with open(filename, 'r') as f:
+        with open(fname, 'r') as f:
             data = json.load(f)
     except Exception as e:
-        print(e)
+        msg = 'JSON parse failed for {}: {}'.format(fname, str(e))
+        print(msg)
+        logger.debug(msg)
         data = None
     return data
 
-def count_keys(d):
+def count_keys(d: dict) -> int:
     """Count number of keys in given dict."""
     return (0 if not isinstance(d, dict) else
             len(d) + sum(count_keys(v) for v in d.values()))
 
-def max_depth(d):
+def max_depth(d: dict) -> int:
     """Return maximum depth of given dict."""
     return (0 if not isinstance(d, dict) or len(d) == 0 else
             1 + max(max_depth(v) for v in d.values()))
 
-def trim(val, n, ellipsis='...'):
+def trim(val, n: int, ellipsis: str='...') -> str:
     """Trim value to max of n chars."""
     val_str = str(val)
     trim_len = max(n - len(ellipsis), 0)
@@ -49,182 +218,20 @@ def join_pair(v1, v2, truncate, sep='\t', left=20, max_len=80):
     return (sep.join([trim(v1, left), trim(v2, right)]) if truncate else
             sep.join([str(v1), str(v2)]))
 
-# Search Functions
-
-def find_key(d, nested_key):
-    """Attempt to find key in dict, where key may be nested key1.key2..."""
-    keys = nested_key.split('.')
-    key = keys[0]
-
-    return (d[key] if len(keys) == 1 and key in d else
-            None if len(keys) == 1 and key not in d else
-            None if not isinstance(d[key], dict) else
-            find_key(d[key], '.'.join(keys[1:])) if key in d else
-            None)
-
-def find_key_rec(search_d, search_key):
-    """Attempt to find all search_key (BFS) in dict, return value and level."""
-    hits = []
-    dicts = [(0, search_d)]
-
-    while dicts:
-        level, d = dicts.pop()
-        for key in d:
-            if key == search_key:
-                hits.append((level, d[key]))
-            else:
-                if isinstance(d[key], dict):
-                    dicts.append((level + 1, d[key]))
-
-    return hits
-
-def get_all_keys(search_d):
-    """Retrieve all keys in dict (BFS) in format key1.key2..."""
-    keys = []
-    dicts = [('', search_d)]
-
-    while dicts:
-        parent, d = dicts.pop()
-        for key in sorted(d.keys()):
-            full_key = key if parent == '' else '.'.join([parent, key])
-            keys.append(full_key)
-            if isinstance(d[key], dict):
-                dicts.append((full_key, d[key]))
-
-    return keys
-
-# Inspection Functions
-
-def describe(data, quiet):
-    """Describe structure of data."""
-    if not quiet:
-        print('\n> Describe structure of file')
+def print_freq_keys(keys: List[str], truncate: bool) -> None:
+    """Print keys with frequency counts."""
+    if keys:
+        keys_ = [trim(key, 70) if truncate else str(key) for key in keys]
+        freq_list = utils.count_freq_list(keys_)
+        print('\n'.join(': '.join([str(elem), str(freq)]) for elem, freq in
+                        sorted(freq_list, key=lambda x: x[0])))
     else:
-        print('')
+        print('Empty input.')
 
-    print('Top-level keys {:,d}'.format(len(data)))
-    print('Total keys     {:,d}'.format(count_keys(data)))
-    print('Max depth      {:,d}'.format(max_depth(data)))
-    print('Total chars    {:,d}'.format(len(json.dumps(data))))
-
-    return True
-
-def sample(data, n, quiet, truncate):
-    """Sample first n (key, value) pairs of file."""
-    if not quiet:
-        print('\n> Sample first {:,d} (key, value) pairs from file'.format(n))
-    else:
-        print('')
-
-    keys = sorted(data.keys())[:n]
-    pairs = [join_pair(key, data[key], truncate) for key in keys]
-    print('\n'.join(pairs))
-    return True
-
-def get_chars(data, n, quiet):
-    """Print first n chars of file."""
-    if not quiet:
-        print('\n> Show first {:,d} chars of file'.format(n))
-    else:
-        print('')
-
-    data_str = json.dumps(data, indent=2, sort_keys=True)
-    print(data_str[:n])
-    return True
-
-def find(data, key, quiet, truncate):
-    """Attempt to find key in dict, where key nesting in form key1.key2..."""
-    if not quiet:
-        print('\n> Find key {} in data'.format(key))
-    else:
-        print('')
-
-    val = find_key(data, key)
-
-    if val is not None:
-        if truncate:
-            print(trim(val, 80))
-        elif isinstance(val, dict):
-            print(json.dumps(val, indent=2, sort_keys=True))
-        else:
-            print(val)
-    else:
-        print('Key not found.')
-    return True
-
-def find_rec(data, key, quiet, truncate):
-    """Find key recursively in data, return all occurences."""
-    if not quiet:
-        print('\n> Find key {} recursively in data'.format(key))
-    else:
-        print('')
-
-    vals = find_key_rec(data, key)
-    if vals is not []:
-        found = [join_pair('Level {:,d}'.format(lvl), val, truncate)
-                 for lvl, val in vals]
-        print('\n'.join(found))
-    else:
-        print('Key not found.')
-    return True
-
-def get_keys(data, recursive, quiet, truncate):
-    """List all top-level keys in data."""
-    if not quiet:
-        if recursive:
-            print('\n> List all keys in data.')
-        else:
-            print('\n> List top-level keys in data.')
-    else:
-        print('')
-
-    keys = get_all_keys(data) if recursive else sorted(data.keys())
-    if keys is not []:
-        if truncate:
-            print('\n'.join([trim(key, 80) for key in keys]))
-        else:
-            print('\n'.join([str(key) for key in keys]))
-    else:
-        print('Empty file.')
-    return
-
-def less(data_str):
+def less(data):
     """Pretty print JSON and pipe to less."""
     p = Popen('less', stdin=PIPE)
-    p.stdin.write(data_str.encode())
+    p.stdin.write(data.encode())
     p.stdin.close()
     p.wait()
-    return True
 
-# Main
-
-def main(args):
-    """Process args from argparse."""
-    data = test_json(args.filename)
-    if not data: return False
-
-    if args.describe:
-        describe(data, args.quiet)
-    if args.sample:
-        sample(data, args.sample, args.quiet, args.truncate)
-    if args.chars:
-        get_chars(data, args.chars, args.quiet)
-    if args.find:
-        find(data, args.find, args.quiet, args.truncate)
-    if args.find_recursive:
-        find_rec(data, args.find_recursive, args.quiet, args.truncate)
-    if args.keys:
-        get_keys(data, False, args.quiet, args.truncate)
-    if args.keys_recursive:
-        get_keys(data, True, args.quiet, args.truncate)
-
-    print('\n')
-
-    other_args = [args.describe, args.sample, args.chars, args.find,
-                  args.find_recursive, args.keys, args.keys_recursive]
-    if args.less or not any(other_args):
-        data_str = json.dumps(data, indent=2, sort_keys=True)
-        print(data_str)
-        less(data_str)
-
-    return True
---
+++ datautils/test/test_jbro.py
@@ -0,0 +1,105 @@
+"""Pytest suite for jbro
+"""
+
+from datautils.json import jbro # type: ignore
+
+################################################################################
+
+class TestSearch:
+    """Test the search functions."""
+
+    def test_find_key(self):
+        """Find key or nested keys in dict, return val."""
+        f = jbro._find_key
+
+        d1 = {'a': 'b'}
+        d2 = {'a': 'b',
+              'c': 'd',
+              'e': {'f': 'g'}}
+        d3 = {'a': {'b': {'c': 0}}}
+
+        assert f(d1, 'a') == 'b'
+        assert f(d2, 'e') == {'f': 'g'}
+        assert f(d2, ['e', 'f']) == 'g'
+        assert f(d3, ['a', 'b', 'c']) == 0
+        assert f(d3, 'e') is None
+        assert f(d3, ['a', 'd']) is None
+
+    def test_find_key_rec(self):
+        """Find key recursively in dict."""
+        f = jbro._find_key_rec
+
+        d0 = {}
+        d1 = {'a': 'b'}
+        d2 = {'a': 'b',
+              'c': 'd',
+              'e': {'a': 'f'}}
+
+        assert f(d0, 'a') == []
+        assert f(d1, 'a') == [(0, 'b')]
+        assert f(d2, 'a') == [(0, 'b'), (1, 'f')]
+
+    def test_get_all_keys(self):
+        """Retrieve all keys in dict"""
+        f = jbro.get_all_keys
+
+        d1 = {'a': '',
+              'b': '',
+              'c': ''}
+        d2 = {'a': 'b',
+              'c': 'd',
+              'e': {'a': 'f'}}
+
+        assert set(f(d1)) == set(['a', 'b', 'c'])
+        assert set(f(d2)) == set(['a', 'c', 'e', 'e.a'])
+
+################################################################################
+
+class TestHelpers:
+    """Test the various helper functions."""
+
+    def test_count_keys(self):
+        """Count # keys in dict."""
+        f = jbro.count_keys
+
+        d0 = {}
+        d1 = {'1': 0}
+        d4 = {'1': {'2': 0},
+              '3': 0,
+              '4': [0, 0, 0]}
+
+        assert f(d0) == 0
+        assert f(d1) == 1
+        assert f(d4) == 4
+
+    def test_max_depth(self):
+        """Find max depth of dict."""
+        f = jbro.max_depth
+
+        d6 = {1: {2: {3: {4: {5: {6: 'a'}}}}}}
+        d0 = {}
+        d1 = {'a': ['b', 1, 2, 3, {'4': '5'}]}
+
+        assert f(d6) == 6
+        assert f(d0) == 0
+        assert f(d1) == 1
+
+    def test_trim(self):
+        """String trimming."""
+        f = jbro.trim
+
+        string = '0123456789abcdefg'
+        assert f(string, 10) == '0123456...'
+        assert f(string, 3) == '...'
+        assert f(string, 0, '') == ''
+        assert f(string, 100) == string
+
+    def test_join_pair(self):
+        """Joining pair to string."""
+        f = jbro.join_pair
+
+        v1, v2 = 'value1', 'value2'
+        sep = '->'
+        assert f(v1, v2, False, sep) == 'value1->value2'
+        assert f(v1, v2, True, sep, 5) == 'va...->value2'
+        assert f(v1, v2, True, sep, 5, 12) == 'va...->va...'
