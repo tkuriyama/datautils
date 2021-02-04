782854bcc865354eefe3bd054f12e8fc2980b11a
tkuriyama <taro.kuriyama@gmail.com>
2021-02-04 16:22:33 -0500

    Adding parameterized generic type

--- datautils/core/utils.py
+++ datautils/core/utils.py
@@ -2,12 +2,14 @@
 """
 
 from collections import defaultdict # type: ignore
-from typing import Any, List, Tuple # type: ignore
+from typing import Any, Dict, List, Tuple, TypeVar # type: ignore
 
 ################################################################################
 # Lists
 
-def count_freq(lst: list) -> dict:
+T = TypeVar('T')
+
+def count_freq(lst: List[T]) -> Dict[T, int]:
     """Return frequency count of elements as dict."""
     d: dict
     d = defaultdict(int)
@@ -15,7 +17,7 @@ def count_freq(lst: list) -> dict:
         d[elem] += 1
     return d
 
-def count_freq_list(lst: list) -> List[Tuple[Any, int]]:
+def count_freq_list(lst: List[T]) -> List[Tuple[T, int]]:
     """Return frequncy count of elements as list of tuples."""
     d = count_freq(lst)
     return list(d.items())
