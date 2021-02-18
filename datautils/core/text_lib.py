bcf9877673be873a0b0e3ec41e9c30f8021c2cd7
tkuriyama <taro.kuriyama@gmail.com>
2021-02-18 10:21:33 -0500

    Adding int and float string parsing

---
+++ datautils/core/text_lib.py
@@ -0,0 +1,42 @@
+"""Text convenience functions.
+"""
+
+from enum import Enum # type: ignore
+import logging # type: ignore
+import datetime as dt # type: ignore
+from typing import Collection, List, Optional
+import re # type: ignore
+
+from datautils.core import log_setup # type: ignore
+
+
+################################################################################
+# Initialize Logging -- set logging level to > 50 to suppress all output
+
+logger = log_setup.init_file_log(__name__, logging.WARNING)
+
+
+################################################################################
+# Numbers
+
+class NumberStyle(Enum):
+    US = 0
+    EU = 1
+
+def str_to_float(n: str, style: NumberStyle = NumberStyle.US) -> float:
+    """Parse string to float."""
+    if style == NumberStyle.US:
+        n = n.replace(',', '')
+    elif style == NumberStyle.EU:
+        n = n.replace('.', '')
+        n = n.replace(',', '.')
+
+    for c in ('\'', '"', ':', ';'):
+        n = n.replace(c, '')
+
+    return float(n.strip())
+
+def str_to_int(n: str, style: NumberStyle = NumberStyle.US) -> int:
+    """Parse string to integer."""
+    return int(str_to_float(n, style))
+
---
+++ tests/test_text_lib.py
@@ -0,0 +1,25 @@
+"""Pytest suite for text_lib.
+"""
+
+from datautils.core import text_lib
+
+class TestNumbers:
+    """Test number functions."""
+
+    def test_str_to_float(self):
+        """Test str_to_float conversion."""
+        f = text_lib.str_to_float
+
+        assert f('218,762.0001') == 218762.0001
+        assert f('218.762,00', style=text_lib.NumberStyle.EU) == 218762.00
+        assert f('218,762') == 218762.0
+        assert f('762') == 762.0
+
+    def test_str_to_int(self):
+        """Test str_to_float conversion."""
+        f = text_lib.str_to_int
+
+        assert f('218,762.0001') == 218762
+        assert f('218.762,00', style=text_lib.NumberStyle.EU) == 218762
+        assert f('218,762') == 218762
+        assert f('762') == 762
