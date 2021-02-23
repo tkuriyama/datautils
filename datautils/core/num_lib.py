f9975b963fb0f7698693161f215ef309cdecb0d9
tkuriyama <taro.kuriyama@gmail.com>
2021-02-23 11:09:08 -0500

    Add windsorize

--- datautils/core/num_lib.py
+++ datautils/core/num_lib.py
@@ -2,7 +2,8 @@
 """
 
 import logging # type: ignore
-from typing import Optional # type: ignore
+import numpy as np # type: ignore
+from typing import Collection, List, Optional, TypeVar # type: ignore
 
 from datautils.core import log_setup # type: ignore
 
@@ -25,3 +26,30 @@ def approx_eq(f1: float,
     """
     diff = abs(epsilon_abs) if epsilon_abs else abs(epsilon * min(f1, f2))
     return abs(f2 - f1) <= diff
+
+
+################################################################################
+# Statistics
+
+T = TypeVar('T')
+Percentile = float
+
+def windsorize(xs: Collection[T],
+               lower: Percentile,
+               upper: Percentile
+               ) -> List[T]:
+    """Windsorize given collection.
+    Percentiles are defined in range [0.0, 100.0].
+    Values are bound by the lower and upper percentile args.
+    """
+    if not approx_eq(lower, 100 - upper):
+        logger.warning('Asymmetric bounds {} and {}'.format(lower, upper))
+    if lower < 1.0 and upper < 1.0:
+        msg = 'Percentiles defined in range [0.0, 100.0], got {}, {}'
+        logger.warning(msg.format(lower, upper))
+
+    low_val = np.percentile(list(xs), lower, interpolation='nearest')
+    up_val = np.percentile(list(xs), upper, interpolation='nearest')
+
+    return [low_val if x < low_val else up_val if x > up_val else x
+            for x in xs]
--- tests/test_num_lib.py
+++ tests/test_num_lib.py
@@ -1,7 +1,7 @@
 """Pytest suite for num_lib.
 """
 
-import numpy as np
+import numpy as np # type: ignore
 
 from datautils.core import num_lib # type: Ignore
 
@@ -21,3 +21,19 @@ class TestFloat:
         assert f(1.0, 0.9, 0.1) is False
         assert f(1.0, 0.99, 0.1) is True
         assert f(np.pi, 3.14159) is True
+
+class TestStatistics:
+    """Test statistics functions."""
+
+    def test_windsorize(self):
+        """Test windsozie."""
+        f = num_lib.windsorize
+
+        xs = [x for x in range(1, 101)]
+        xs_set = set(xs)
+
+        xs_ = f(xs, 5, 95)
+        assert all([x >= 5 and x <= 95 for x in xs_]) is True
+
+        xs_set_ = f(xs_set, 5, 95)
+        assert all([x >= 5 and x <= 95 for x in xs_set_]) is True
